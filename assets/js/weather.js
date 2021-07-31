e36aa58fbb6c4996df832fc97069d52790b77332
Ven Kim <venvex@gmail.com>
2021-07-31 16:30:51 -0400

    created click event for search; created storage and li elements for previous searches using JSON

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,5 +1,44 @@
 $(document).ready(function() {
-    var date = moment().format('LLL');
+	var date = moment().format('LLL');
 
-    return console.log(date);
+	console.log(date);
+
+	function storeCities() {
+		$('.past-cities').empty()
+		
+		var recentCities = JSON.parse(localStorage.getItem('cities')) || []
+		for (var i = 0; i < recentCities.length; i++) {
+			while (recentCities.length > 5) {
+				var lastFive = recentCities.length - 5;
+				var index = 0;
+				recentCities.splice(index, lastFive);
+				index++
+			}
+
+			var newCity = $('<li>')
+			newCity.addClass("list-group-item");
+			newCity.text(recentCities[i].name)
+			$('.past-cities').append(newCity);
+		}
+	}
+	storeCities();
+
+	$(".button").on("click", function(event) {
+		event.preventDefault();
+
+		var city = $('.form-control').val()
+		var cityList = [];
+
+		var recentCities = JSON.parse(localStorage.getItem('cities')) || []
+		$('.past-cities').val(recentCities);
+		var savedCity = {
+			name: city
+		};
+
+		recentCities.push(savedCity);
+		localStorage.setItem('cities', JSON.stringify(recentCities));
+
+		storeCities();
+		search(city);
+	})
 })
