77186f02a91fa64cce1d03f32b8b8f65ef97d0a9
Ven Kim <venvex@gmail.com>
2021-08-01 14:43:47 -0400

    creating and testing the current-city forecast and returns

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -41,4 +41,43 @@ $(document).ready(function() {
 		storeCities();
 		search(city);
 	})
+
+	$('.past-cities').on("click", "li", function(event) {
+		event.preventDefault();
+		var city = $(this).text();
+		search(city);
+	})
+
+	function search(city) {
+
+		$(".days").show()
+		var queryURL1 = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7eacf5331e789eb97f9990d729cb2139'
+		
+		$.when(
+			$.ajax({
+				url: queryURL1,
+				method: "GET"
+
+			}))
+			.then(function(response) {
+				var latitude = response.coord.lat;
+				var longititude = response.coord.lon;
+				var queryURL2 = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longititude + '&appid=7eacf5331e789eb97f9990d729cb2139'
+				
+				$.ajax({
+					url:queryURL2,
+					method: "GET"
+				})
+					.then(function(response2) {
+						$('.city-name').empty();
+
+						var cityName = response.name;
+						var temp = (response.main.temp - 273.15) * 9 / 5 + 32
+						var fahrenheit = $('<p>');
+						var humidity = $('<p>');
+						var windSpeed = response.wind.speed * 2.236936;
+					})
+			})
+	}
+
 })
